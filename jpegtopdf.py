6a8232c1f4aecc147d70b1a8efa8a4ef09260830
ArdaOzcan <34956347+ArdaOzcan@users.noreply.github.com>
2020-04-15 13:09:11 +0300

    Update jpegtopdf.py
    - Added open_with_correct_rotation(). Despite it's long name it's very useful. PIL rotates some JPEG images when saving them. I found out that it was related to JPEG compression and the orientation tag was stored in exif. So open_with_correct_rotation() checks the exif data and rotates the image accordingly (Note that i have only tested this with orientation 6, the rest were looked up from [here](https://magnushoff.com/articles/jpeg-orientation/))
    -Created temp_cleanup() function and the cleanup is made at the end of the program now.

--- jpegtopdf.py
+++ jpegtopdf.py
@@ -4,10 +4,11 @@ import argparse
 import datetime
 
 from fpdf import FPDF
-from PIL import Image
+from PIL import Image, ExifTags
 
 action_index = 0
 verbose = False
+
 
 def log(msg):
     global action_index
@@ -17,14 +18,32 @@ def log(msg):
         print(f'[{"{:05d}".format(action_index)}][{date}]: {msg}')
     action_index += 1
 
+
 def compressed_image_name(old_name):
     return os.path.join('temp', f'cmp_{old_name}')
 
 
-def compress(dir, file):
-    picture = Image.open(os.path.join(dir, file))
-    picture.save(compressed_image_name(file), 'JPEG', optimize=True, quality=85)
-    
+def open_with_correct_rotation(im_dir, fp):
+    picture = Image.open(os.path.join(im_dir, fp))
+    for orientation in ExifTags.TAGS.keys():
+        if ExifTags.TAGS[orientation] == 'Orientation':
+            break
+
+    if picture._getexif()[orientation] == 3:
+        picture = picture.transpose(Image.ROTATE_180)
+    elif picture._getexif()[orientation] == 6:
+        picture = picture.transpose(Image.ROTATE_270)
+    elif picture._getexif()[orientation] == 8:
+        picture = picture.transpose(Image.ROTATE_90)
+
+    return picture
+
+
+def compress(im_dir, file):
+    picture = open_with_correct_rotation(im_dir, file)
+    picture.save(compressed_image_name(file),
+                 'JPEG', optimize=True, quality=85)
+
 
 def check_temp_folder():
     log('CHECKING IF \\temp EXISTS')
@@ -32,41 +51,58 @@ def check_temp_folder():
         os.mkdir('temp')
         log('CREATING \\temp')
 
-def create_pdf(pdf_file_name, list_images, dir = ''):
-    check_temp_folder()
-    
-    if not pdf_file_name.endswith('.pdf'):
-        pdf_file_name += '.pdf'
-        
 
-    cover = Image.open(os.path.join(dir, str(list_images[0])))
-    width, height = cover.size
-
-    pdf = FPDF(unit = "pt", format = [width, height])
-
+def temp_cleanup(list_images):
     for page in list_images:
-        pdf.add_page()
-        log(f'COMPRESSING {page}')
-        compress(dir, page)
-        pdf.image(compressed_image_name(str(page)), 0, 0)
-        log(f'ADING {compressed_image_name(str(page))}')
         os.remove(compressed_image_name(page))
         log(f'REMOVING {compressed_image_name(str(page))}')
 
+
+def create_pdf(pdf_file_name, list_images, dir=''):
+    check_temp_folder()
+
+    if not pdf_file_name.endswith('.pdf'):
+        pdf_file_name += '.pdf'
+
+    try:
+        with Image.open(compressed_image_name(str(list_images[0]))) as cover:
+            width, height = cover.size
+    except FileNotFoundError:
+        compress(dir, list_images[0])
+        log(f'COMPRESSING {list_images[0]}')
+        with Image.open(compressed_image_name(str(list_images[0]))) as cover:
+            width, height = cover.size
+
+    pdf = FPDF(unit="pt", format=[width, height])
+
+    for page in list_images:
+        pdf.add_page()
+
+        try:
+            pdf.image(compressed_image_name(str(page)), 0, 0)
+        except RuntimeError:
+            compress(dir, page)
+            log(f'COMPRESSING {page}')
+            pdf.image(compressed_image_name(str(page)), 0, 0)
+
+        log(f'ADDING {compressed_image_name(str(page))}')
+
     pdf.output(os.path.join(os.getcwd(), pdf_file_name), 'F')
     log(f'EXPORTING TO {os.path.join(os.getcwd(), pdf_file_name)}')
-    
+
+
 if __name__ == '__main__':
-        
+
     parser = argparse.ArgumentParser()
     parser.add_argument('pdf_file_name')
     parser.add_argument('-d', '--images_dir_path')
-    parser.add_argument('-l','--image_list', nargs='+', required=True)
+    parser.add_argument('-l', '--image_list', nargs='+', required=True)
     parser.add_argument('-v', '--verbose', action='store_true', default=False)
-    
+
     args = parser.parse_args()
     if args.images_dir_path is None:
         args.images_dir_path = os.getcwd()
-    
+
     verbose = args.verbose
     create_pdf(args.pdf_file_name, args.image_list, args.images_dir_path)
+    temp_cleanup(args.image_list) # If called by command-line, cleanup here. If using UI, cleanup is done in closeEvent()
